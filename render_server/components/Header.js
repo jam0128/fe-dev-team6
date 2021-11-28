1d45cc189490d373048f47eee823277a0a1c0829
Juny0ung <joonchoi518@kaist.ac.kr>
2021-11-28 21:45:56 +0900

    Header style

--- render_server/components/Header.js
+++ render_server/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Input, Grid } from 'semantic-ui-react';
+import "../styles/Header.css";
 
 
 function Header(props) {
     console.log(props)
    return (
-       <div>
+       <div style={{fontWeight: "900"}}>
             <Input 
                 fluid
                 icon='search'
@@ -14,21 +15,21 @@ function Header(props) {
                 labelPosition='left'
                 size='huge'
             />
-            <Grid columns={14} padded>
-                <Grid.Column>통합</Grid.Column>
-                <Grid.Column>이미지</Grid.Column>
-                <Grid.Column>VIEW</Grid.Column>
-                <Grid.Column>지식iN</Grid.Column>
-                <Grid.Column>인플루언서</Grid.Column>
-                <Grid.Column>동영상</Grid.Column>
-                <Grid.Column>쇼핑</Grid.Column>
-                <Grid.Column>뉴스</Grid.Column>
-                <Grid.Column>어학사전</Grid.Column>
-                <Grid.Column>지도</Grid.Column>
-                <Grid.Column>책</Grid.Column>
-                <Grid.Column>뮤직</Grid.Column>
-                <Grid.Column>지식백과</Grid.Column>
-                <Grid.Column>오디오클립</Grid.Column>
+            <Grid columns={14} padded style={{width:"max-content"}}>
+                <Grid.Column className="header_main" style={{width:"max-content", color:"#06c755", borderBottom:"4px solid"}}>통합</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>이미지</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>VIEW</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>지식iN</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>인플루언서</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>동영상</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>쇼핑</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>뉴스</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>어학사전</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>지도</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>책</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>뮤직</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>지식백과</Grid.Column>
+                <Grid.Column style={{width:"max-content"}}>오디오클립</Grid.Column>
             </Grid>
        </div>
    )

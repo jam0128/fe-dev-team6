b898b58e79b5e246411968646e016de5d38ecac3
Juny0ung <joonchoi518@kaist.ac.kr>
2021-11-29 01:55:36 +0900

    style variance

--- render_server/components/Header.js
+++ render_server/components/Header.js
@@ -4,7 +4,9 @@ import "../styles/Header.css";
 
 
 function Header(props) {
-    console.log(props)
+    const maxCo = {
+        width: "max-content"
+    };
    return (
        <div style={{fontWeight: "900", backgroundColor:"#fff"}}>
             <Input 
@@ -16,20 +18,20 @@ function Header(props) {
                 size='huge'
             />
             <Grid columns={14} padded style={{width:"max-content"}}>
-                <Grid.Column className="header_main" style={{width:"max-content", color:"#06c755", borderBottom:"4px solid"}}>통합</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>이미지</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>VIEW</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>지식iN</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>인플루언서</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>동영상</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>쇼핑</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>뉴스</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>어학사전</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>지도</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>책</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>뮤직</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>지식백과</Grid.Column>
-                <Grid.Column style={{width:"max-content"}}>오디오클립</Grid.Column>
+                <Grid.Column className="header_main" style={{...maxCo, color:"#06c755", borderBottom:"4px solid"}}>통합</Grid.Column>
+                <Grid.Column style={maxCo}>이미지</Grid.Column>
+                <Grid.Column style={maxCo}>VIEW</Grid.Column>
+                <Grid.Column style={maxCo}>지식iN</Grid.Column>
+                <Grid.Column style={maxCo}>인플루언서</Grid.Column>
+                <Grid.Column style={maxCo}>동영상</Grid.Column>
+                <Grid.Column style={maxCo}>쇼핑</Grid.Column>
+                <Grid.Column style={maxCo}>뉴스</Grid.Column>
+                <Grid.Column style={maxCo}>어학사전</Grid.Column>
+                <Grid.Column style={maxCo}>지도</Grid.Column>
+                <Grid.Column style={maxCo}>책</Grid.Column>
+                <Grid.Column style={maxCo}>뮤직</Grid.Column>
+                <Grid.Column style={maxCo}>지식백과</Grid.Column>
+                <Grid.Column style={maxCo}>오디오클립</Grid.Column>
             </Grid>
        </div>
    )

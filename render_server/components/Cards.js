eeef04dc249e666797726eb199d01612eb886bcf
Juny0ung <joonchoi518@kaist.ac.kr>
2021-11-30 18:24:20 +0900

    tabs font

--- render_server/components/Cards.js
+++ render_server/components/Cards.js
@@ -38,28 +38,30 @@ function CardsComponent(props) {
             fontWeight: "300"
         }}>{editTitle(card.title)}</Card.Header>)
 
+        const writerBasicStyle = {
+            fontSize: "12px", 
+            color: "#8f8f8f", 
+            paddingTop: "0px",
+            paddingBottom: "0px"
+        };
+
         switch (props.form) {
             case "works" :
-                content.push(<Card.Meta style={{marginTop:"5px", marginBottom:"0px"}}>{card.role}</Card.Meta>);
-                content.push(<Card.Meta style={{marginTop:"1px"}}>{card.date}</Card.Meta>);
+                content.push(<Card.Meta style={{...writerBasicStyle, fontWeight:"100", marginTop:"5px", marginBottom:"0px"}}>{card.role}</Card.Meta>);
+                content.push(<Card.Meta style={{...writerBasicStyle, fontWeight:"100", marginTop:"1px"}}>{card.date}</Card.Meta>);
                 break;
             case "videos" :
-                const writerBasicStyle = {
-                    fontSize: "12px", 
-                            color: "#8f8f8f", 
-                            width: "max-content", 
-                            paddingTop: "0px",
-                            paddingBottom: "0px"
-                };
                 
                 content.push(<Card.Meta>
                     <Grid columns={2} divided style={{marginTop:"0px"}}>
                         <Grid.Column style={{
                             ...writerBasicStyle,
+                            width: "max-content", 
                             paddingRight: "5px"
                         }}>{card.website}</Grid.Column>
                         <Grid.Column style={{
                             ...writerBasicStyle,
+                            width: "max-content", 
                             paddingLeft: "5px"
                         }}>{card.date}</Grid.Column>
                     </Grid>

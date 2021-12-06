00d871bc601928dab973dd7fb5f43ea6a0f0f21e
Juny0ung <joonchoi518@kaist.ac.kr>
2021-12-06 22:19:39 +0900

    App.js

--- render_server/components/App.js
+++ render_server/components/App.js
@@ -15,18 +15,35 @@ function App(props) {
     }
 
     return(
-        <div style={{backgroundColor:"#e9ecef"}}>
-            <Header value={props.info['test1Key']}/>
-            <div style={{
-                width:'768px', 
-                margin:'0 auto', 
-                marginTop:"15px"
-            }}>
-                <KeyWordComponent info={props.info['compo1']['data']}/>
-                {constOthers(props.info['compo2'])}
-            </div>
-            <Footer/>
-        </div>
+        <html lang="en">
+            <head>
+                <title>Naver</title>
+                <link
+                async
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+                />
+                <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js"></script>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width,initial-scale=1"/>  
+            </head>
+            <body>
+                <div style={{backgroundColor:"#e9ecef"}}>
+                    <Header value={props.info['test1Key']}/>
+                    <div style={{
+                        width:'768px', 
+                        margin:'0 auto', 
+                        marginTop:"15px"
+                    }}>
+                        <KeyWordComponent info={props.info['compo1']['data']}/>
+                        {constOthers(props.info['compo2'])}
+                    </div>
+                    <Footer/>
+                </div>
+            </body>
+        </html>
     )
 }
 
--- render_server/routes/index.js
+++ render_server/routes/index.js
@@ -8,29 +8,7 @@ var router = express.Router();
 router.post('/', function(req, res, next) {    
     let result = `
     <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <title>Naver</title>
-            <link
-            async
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-            />
-            <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js"></script>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width,initial-scale=1">  
-        </head>
-        <body>
-            <div id="root"> ${ReactDOMServer.renderToString((<App info={req.body}/>))} </div>
-        </body>
-        <script type="text/babel">
-            const ReactDOM = require("react-dom");
-            const App = require("../components/App").default;
-            ReactDOM.hydrate(<App info=${req.body}/>, documnet.getElementById('root'));
-        </script>
-    </html>
+    ${ReactDOMServer.renderToString((<App info={req.body}/>))}
     `;
     
     res.send(result);
